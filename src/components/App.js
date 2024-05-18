f782e2f5efba860a219490487e8d2ec6023119a2
warishshahbaz <mdshahbazwarish1996@gmail.com>
2024-05-18 17:08:42 +0530

    impl

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,8 @@
-
 import React from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+  return <div>kooo</div>;
+};
 
-export default App
+export default App;
